b99bc4af363731af9534a11ad97014ce93d5d5f1
magwindow <antosha.petrakov@yandex.ru>
2025-10-18 17:30:14 +0300

    Get images name

--- requests_example/req1.py
+++ requests_example/req1.py
@@ -4,8 +4,8 @@ import requests
 def download_image(url):
     r = requests.get(url, stream=True)
     if r.status_code == 200:
-        with open('image.jpg', 'wb') as f:
+        with open(url.split('/')[-1], 'wb') as f:
             r.raw.decode_content = True
             shutil.copyfileobj(r.raw, f)
 
-# download_image('https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')
+download_image('https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')
