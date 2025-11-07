0a13c358445ac5a5adbc5d61c0430b9fc24e85ff
magwindow <antosha.petrakov@yandex.ru>
2025-11-07 10:03:30 +0300

    update url import

--- urlimport.py
+++ urlimport.py
@@ -151,6 +151,79 @@ class UrlPackageLoader(UrlModuleLoader):
     def is_package(self, fullname):
         return True
     
+
+# Класс для поиска пути в URL
+class UrlPathFinder(importlib.abc.PathEntryFinder):
+    def __init__(self, baseurl):
+        self._links = None
+        self._loader = UrlModuleLoader(baseurl)
+        self._baseurl = baseurl
+        
+    def find_loader(self, fullname):
+        log.debug('find_loader: %r', fullname)
+        parts = fullname.split('.')
+        basename = parts[-1]
+        
+        # Проверка кеша ссылок
+        if self._links is None:
+            self._links = [] 
+            self._links = _get_links(self._baseurl)
+            
+        # Проверить, пакет ли это
+        if basename in self._links:
+            log.debug('find_loader: trying package %r', fullname)
+            fullurl = self._baseurl + '/' + basename
+            # Попытка загрузить пакет (которая обращается к __init__.py)
+            loader = UrlPackageLoader(fullurl)
+            
+            try:
+                loader.load_module(fullname)
+                log.debug('find_loader: package %r loaded', fullname)
+            except ImportError as e:
+                log.debug('find_loader: %r is a namespace package', fullname)
+                loader = None
+            return (loader, [fullurl])
+
+        # Обычный модуль
+        filename = basename + '.py'
+        
+        if filename in self._links:
+            log.debug('find_loader: module %r found', fullname)
+            return (self._loader, [])
+        else:
+            log.debug('find_loader: module %r not found', fullname)
+            return (None, [])
+
+    def invalidate_caches(self):
+        log.debug('invalidating link cache')
+        self._links = None
+
+# Проверка пути на предмет того, выглядит ли он как URL
+_url_path_cache = {}
+
+def handle_url(path):
+    if path.startswith(('http://', 'https://')):
+        log.debug(f'Handle path? {path}. [Yes]')
+        if path in _url_path_cache:
+            finder = _url_path_cache[path]
+        else:
+            finder = UrlPathFinder(path)
+            _url_path_cache[path] = finder
+        return finder
+    else:
+        log.debug(f'Handle path? {path}. [No]')
+        
+def install_path_hook():
+    sys.path_hooks.append(handle_url)
+    sys.path_importer_cache.clear()
+    log.debug('Installing handle_url')
+    
+def remove_path_hook():
+    sys.path_hooks.remove(handle_url)
+    sys.path_importer_cache.clear()
+    log.debug('Removing handle_url')
+
+
 # Вспомогательные функции для инсталляции/деинсталляции загрузчика
 _installed_meta_cache = {}
 
