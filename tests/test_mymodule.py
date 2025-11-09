38523ea959d450467b8068f5aa6a6f0e3b838614
magwindow <antosha.petrakov@yandex.ru>
2025-11-09 10:41:42 +0300

    unitests

---
+++ tests/test_mymodule.py
@@ -0,0 +1,20 @@
+from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
+import unittest
+import utils.mymodule as mymodule
+
+class TestURLPrint(TestCase):
+    def test_url_gets_to_stdout(self):
+        protocol = 'http'
+        host = 'www'
+        domain = 'example.com'
+        expected_url = f'{protocol}://{host}.{domain}\n'
+        
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            mymodule.urlprint(protocol, host, domain)
+            self.assertEqual(fake_out.getvalue(), expected_url)
+            
+
+if __name__ == '__main__':
+    unittest.main()
